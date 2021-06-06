dd5f87f4fdf929fa75703d08717e79adc4946429
Mateus <mateusowmedeiros@gmail.com>
2021-06-06 02:00:19 -0300

    Full C.R.U.D. and controller run

--- src/main/java/br/com/projeto/usuario/api/controller/UserController.java
+++ src/main/java/br/com/projeto/usuario/api/controller/UserController.java
@@ -1,13 +1,14 @@
 package br.com.projeto.usuario.api.controller;
 
 import br.com.projeto.usuario.api.controller.dto.UserRequestDTO;
+import br.com.projeto.usuario.api.controller.dto.UserResponseDTO;
+import br.com.projeto.usuario.api.controller.mapper.UserMapper;
 import br.com.projeto.usuario.api.entities.User;
 import br.com.projeto.usuario.api.services.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -15,20 +16,41 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user")
-    public void listAllUser(){
-
-    }
-
-    @GetMapping("/user/{name}")
-    public void findUserByName(){
-
-    }
-
     @PostMapping("/user")
-    public User createUser(@RequestBody UserRequestDTO userRequestDTO){
-        var entity = userService.createUser(user);
-        return entity;
+    public UserResponseDTO createUser(@RequestBody UserRequestDTO userRequestDTO){
+        var entity = UserMapper.toEntity(userRequestDTO);
+        var result = userService.createUser(entity);
+        return UserMapper.toDTO(result);
     }
 
+    @GetMapping("/user")
+    public List<UserResponseDTO> listAllUser(){
+        var result = userService.findAll();
+        return UserMapper.toDTOs(result);
+    }
+
+    @GetMapping("/user/name/{name}")
+    public List<UserResponseDTO> findUserByName(@RequestParam String nameRequest){
+        var result = userService.findByName(nameRequest);
+        return UserMapper.toDTOs(result);
+    }
+
+    @GetMapping("/user/email/{email}")
+    public UserResponseDTO findUserByEmail(@RequestParam String emailRequest){
+        var result = userService.findByEmail(emailRequest);
+        return UserMapper.toDTO(result);
+    }
+
+    @PatchMapping("/user/age")
+    public UserResponseDTO updateUser(@RequestBody UserRequestDTO userRequestDTO){
+        var entity = UserMapper.toEntity(userRequestDTO);
+        var result = userService.updateUser(entity);
+        return UserMapper.toDTO(result);
+    }
+
+    @DeleteMapping("/user/delete/{email}")
+    public void deleteUserByEmail(@RequestParam String emailRequest){
+        userService.deleteUser(emailRequest);
+
+    }
 }
--- src/main/java/br/com/projeto/usuario/api/repositories/UserRepository.java
+++ src/main/java/br/com/projeto/usuario/api/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import br.com.projeto.usuario.api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByName(String name);
+    List<User> findByName(String name);
 
+    User findByEmail(String emailRequest);
 }
