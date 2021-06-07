dba71306e408e322cc855847edcb2def04f3fc8f
Mateus <mateusowmedeiros@gmail.com>
2021-06-07 01:15:22 -0300

    Templates Thymeleaf for page, index and register/edit user

--- src/main/java/br/com/projeto/usuario/api/controller/UserController.java
+++ src/main/java/br/com/projeto/usuario/api/controller/UserController.java
@@ -3,10 +3,11 @@ package br.com.projeto.usuario.api.controller;
 import br.com.projeto.usuario.api.controller.dto.UserRequestDTO;
 import br.com.projeto.usuario.api.controller.dto.UserResponseDTO;
 import br.com.projeto.usuario.api.controller.mapper.UserMapper;
-import br.com.projeto.usuario.api.entities.User;
 import br.com.projeto.usuario.api.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -16,40 +17,57 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/")
+    public ModelAndView users(Model model) {
+        model.addAttribute("userList", listAllUser());
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/register")
+    public ModelAndView newUser(@ModelAttribute("user") UserRequestDTO userRequestDTO) {
+        return new ModelAndView("register/user");
+    }
+
+    @PostMapping("/register/user/save")
+    public ModelAndView userSave(@ModelAttribute("user") UserRequestDTO userRequestDTO, Model model) {
+        model.addAttribute("userSave", createUser(userRequestDTO));
+        return new ModelAndView("register/save");
+    }
+
     @PostMapping("/user")
-    public UserResponseDTO createUser(@RequestBody UserRequestDTO userRequestDTO){
+    public UserResponseDTO createUser(@RequestBody UserRequestDTO userRequestDTO) {
         var entity = UserMapper.toEntity(userRequestDTO);
         var result = userService.createUser(entity);
         return UserMapper.toDTO(result);
     }
 
     @GetMapping("/user")
-    public List<UserResponseDTO> listAllUser(){
+    public List<UserResponseDTO> listAllUser() {
         var result = userService.findAll();
         return UserMapper.toDTOs(result);
     }
 
     @GetMapping("/user/name/{name}")
-    public List<UserResponseDTO> findUserByName(@RequestParam String nameRequest){
+    public List<UserResponseDTO> findUserByName(@RequestParam String nameRequest) {
         var result = userService.findByName(nameRequest);
         return UserMapper.toDTOs(result);
     }
 
     @GetMapping("/user/email/{email}")
-    public UserResponseDTO findUserByEmail(@RequestParam String emailRequest){
+    public UserResponseDTO findUserByEmail(@RequestParam String emailRequest) {
         var result = userService.findByEmail(emailRequest);
         return UserMapper.toDTO(result);
     }
 
     @PatchMapping("/user/age")
-    public UserResponseDTO updateUser(@RequestBody UserRequestDTO userRequestDTO){
+    public UserResponseDTO updateUser(@RequestBody UserRequestDTO userRequestDTO) {
         var entity = UserMapper.toEntity(userRequestDTO);
         var result = userService.updateUser(entity);
         return UserMapper.toDTO(result);
     }
 
     @DeleteMapping("/user/delete/{email}")
-    public void deleteUserByEmail(@RequestParam String emailRequest){
+    public void deleteUserByEmail(@RequestParam String emailRequest) {
         userService.deleteUser(emailRequest);
 
     }
